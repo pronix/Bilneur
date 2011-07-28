d95da5fdc105917d7e085b704af2fce984b9f2fd
parallel588 <parallel588@gmail.com>
2011-07-28 17:03:08 +0400

    added EAN code to product

---
+++ shop/app/models/product_decorator.rb
@@ -0,0 +1,27 @@
+Product.class_eval do
+
+  # associations
+  #
+
+  # scopes
+  #
+
+  # validates
+  #
+  validates :ean, :presence => true, :uniqueness => true
+
+  # callbacks
+  #
+
+  # instance methods
+  #
+
+
+
+  # class methods
+  #
+  class << self
+
+  end
+
+end

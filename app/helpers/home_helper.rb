59372adb746d96dce4d9fe1d189a915dfce76065
ezo <ezo@alisa.(none)>
2011-08-04 15:10:27 +0300

    Fix Start Selling link. Top link too.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,16 +1,12 @@
 module HomeHelper
   def start_selling_link
-    # Chose what link we display fro non registered user, registerd and saller
-    # TODO: Fix it's ugly code
-    return link_to image_tag('forms/strt_slng_btn.png'), new_user_registration_path(:as_seller => true), :class => "btn" if !current_user
-    if current_user.has_role?("seller")
-      link = '/adsdsadasda'
-    elsif current_user.has_role?("user") && ! current_user.has_role?("seller")
-      link = account_path(:as_seller => true)
-    else
-      link new_user_registration_path(:as_seller => true)
-    end
-    link_to image_tag('forms/strt_slng_btn.png'), link, :class => "btn"
+    # Link state. 
+    # If non register -> register with checkbox as_seller
+    # If register and has role user -> account page and add role sale
+    # If already saller role -> now is root_path letter to create product
+    return new_user_registration_path(:as_seller => true) if !current_user
+    return account_path(:as_seller => true) if current_user.has_role?("user") && ! current_user.has_role?("seller")
+    return root_path if current_user.has_role?("seller")
   end
 
 end
--- features/non_register_homepage.feature
+++ features/non_register_homepage.feature
@@ -2,12 +2,13 @@ Feature: User behavior on home page
 
 Background:
   Given I am non auth user
-
+@wip
   Scenario: What I should see in home page
     When I go to the home page
     Then I should see given link with path
-      | link    | link_path        | options        |
-      | Sing Up | the sign up page |                |
-      | Log In  | the sign in page |                |
-      |         | the sign up page | as_seller=true |
+      | link          | link_path        | options        |
+      | Sing Up       | the sign up page |                |
+      | Log In        | the sign in page |                |
+      |               | the sign up page | as_seller=true |
+      | Start Selling | the sign up page | as_seller=true |
       

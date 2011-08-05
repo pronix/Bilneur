2ee88cdd659d659a08dae11166bdea29c0ed796b
parallel588 <parallel588@gmail.com>
2011-08-05 20:34:55 +0400

    editing seller terms and added editing user name

--- app/controllers/dashboard/users_controller.rb
+++ app/controllers/dashboard/users_controller.rb
@@ -1,5 +1,8 @@
 class Dashboard::UsersController < Dashboard::ApplicationController
 
+  helper Admin::BaseHelper
+  helper Admin::NavigationHelper
+
   def show
 
   end
@@ -8,6 +11,9 @@ class Dashboard::UsersController < Dashboard::ApplicationController
   end
 
   def update
+    if current_user.update_attributes(params[:user])
+      flash.notice = "Account updated."
+    end
     redirect_to dashboard_account_path
   end
 

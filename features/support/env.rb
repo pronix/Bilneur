689f69450c929ef214e56f0a0e150caf60e9b554
parallel588 <parallel588@gmail.com>
2011-08-07 14:28:45 +0400

    changed facebook connection

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,25 +6,25 @@ Spork.prefork do
   # newer version of cucumber-rails. Consider adding your own code to a new file
   # instead of editing this one. Cucumber will automatically load all features/**/*.rb
   # files.
-  
+
   require 'cucumber/rails'
   require 'spree_core/testing_support/factories'
-  
+
   # require File.expand_path('vendor/spree-exts/spree-0.60.1/spree_core/../features/support/env', __FILE__)
   # require 'spree_core/testing_support/factories'
-  
+
   Spree::Auth::Config.set(:registration_step => true)
   Spree::Auth::Config.set(:signout_after_password_change => false)
   # sometimes tests fail randomly because cache is not refreshed, fixed that
   Spree::Config.set(:foo => "bar")
-  
-  
+
+
   # Capybara defaults to XPath selectors rather than Webrat's default of CSS3. In
   # order to ease the transition to Capybara we set the default here. If you'd
   # prefer to use XPath just remove this line and adjust any selectors in your
   # steps to use the XPath syntax.
   Capybara.default_selector = :css
-  
+
   # By default, any exception happening in your Rails application will bubble up
   # to Cucumber so that your scenario will fail. This is a different from how
   # your application behaves in the production environment, where an error page will
@@ -41,7 +41,7 @@ Spork.prefork do
   # recommended as it will mask a lot of errors for you!
   #
   ActionController::Base.allow_rescue = false
-  
+
   Capybara.save_and_open_page_path = File.join(Rails.root, "tmp")
   # Remove/comment out the lines below if your app doesn't have a database.
   # For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
@@ -50,7 +50,7 @@ Spork.prefork do
   rescue NameError
     raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
   end
-  
+
   # You may also want to configure DatabaseCleaner to use different strategies for certain features and scenarios.
   # See the DatabaseCleaner documentation for details. Example:
   #
@@ -63,4 +63,26 @@ Spork.prefork do
   #   end
   #
   require 'factory_girl/step_definitions'
+
+  Before('@facebook') do
+    require 'oa-core'
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:facebook] = {
+      'uid' => '111xxx222yyy333zzz',
+      'provider'   => 'facebook',
+      'link'       => 'http://facebook.com/bubbleboy',
+      'email'      => 'boy@bubble.com',
+      'first_name' => 'User',
+      'last_name'  => 'Name',
+      'user_info'  => {
+        'nickname' => "bob"
+      }
+    }
+
+
+    authentication_method = AuthenticationMethod.create!(:environment => "test", :active => true)
+    Preference.create! :owner => authentication_method, :name => "provider", :value => "facebook"
+    Preference.create! :owner => authentication_method, :name => "enable_authentication_method", :value => true
+  end
+
 end

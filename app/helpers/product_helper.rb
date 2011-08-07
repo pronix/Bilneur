92c9a08123f4a03029fb96f00f6728ca364983d0
Maxim Kremenev <ezotrank@gmail.com>
2011-08-07 18:09:13 +0300

    Fix show_star

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -3,9 +3,10 @@ module ProductHelper
   # yel_str = Rating start
   # grey_str = Non rating star
   def show_stars(product, yel_str='icons/yel_str.png',grey_str='icons/grey_str.png',html_wrapper="<i>")
-    wrapper = html_wrapper + 'PASTE_HERE' + html_wrapper.gsub(/^</, '</')
-    yel_star = wrapper.gsub('PASTE_HERE', "#{image_tag yel_str}")
-    grey_star = wrapper.gsub('PASTE_HERE', "#{image_tag grey_str}")
+    html_wrapper_end = html_wrapper.gsub(/^</, '</')
+    # Define yellow and grey star image link
+    yel_star = html_wrapper + image_tag(yel_str) + html_wrapper_end
+    grey_star = html_wrapper + image_tag(grey_str) + html_wrapper_end
     # First we want know ratting of product with yellow.
     out = yel_star * product.avg_rating.to_i
     out += grey_star * (5 - product.avg_rating.to_i)

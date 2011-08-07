3b509e27c14de87a3690353718e0283c3da7b67c
Maxim Kremenev <ezotrank@gmail.com>
2011-08-07 16:30:51 +0300

    Change display star on /t/categories use show_stars helper from product helper.

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -1,8 +1,11 @@
 module ProductHelper
   # Show stars of rating product
-  def show_stars(product, yel_str='icons/yel_str.png',grey_str='icons/grey_str.png')
-    yel_star = "<i>#{image_tag yel_str}</i>"
-    grey_star = "<i>#{image_tag grey_str}</i>"
+  # yel_str = Rating start
+  # grey_str = Non rating star
+  def show_stars(product, yel_str='icons/yel_str.png',grey_str='icons/grey_str.png',html_wrapper="<i>")
+    wrapper = html_wrapper + 'PASTE_HERE' + html_wrapper.gsub(/^</, '</')
+    yel_star = wrapper.gsub('PASTE_HERE', "#{image_tag yel_str}")
+    grey_star = wrapper.gsub('PASTE_HERE', "#{image_tag grey_str}")
     # First we want know ratting of product with yellow.
     out = yel_star * product.avg_rating.to_i
     out += grey_star * (5 - product.avg_rating.to_i)

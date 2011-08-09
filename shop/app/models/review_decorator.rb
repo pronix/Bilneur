e31bfaa9fc6a32882aa006a746caf1f72e7c638a
Maxim Kremenev <ezotrank@gmail.com>
2011-08-09 17:12:55 +0300

    Add last user write review

--- shop/app/models/review_decorator.rb
+++ shop/app/models/review_decorator.rb
@@ -1,7 +1,11 @@
 Review.class_eval do
   before_validation :name_as_register_user
 
+  # Return last 3 review
+  scope :last_reviews, lambda{ |count| approved.order("created_at DESC").first(count) }
 
+  # Return user name, if review was create as anonymous ve user name from Review
+  # Else using firstname and lastname from User model with some fix
   def simple_username
     return user.firstname + ' ' + user.lastname.first + '.' if !user.nil?
     name

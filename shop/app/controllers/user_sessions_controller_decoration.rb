73c6bba1dba60b96ef2afb5bb7cabd1013b19885
parallel588 <parallel588@gmail.com>
2011-08-14 18:49:38 +0400

    fix typo

--- shop/app/controllers/user_sessions_controller_decoration.rb
+++ shop/app/controllers/user_sessions_controller_decoration.rb
@@ -1,12 +1,9 @@
 UserSessionsController.class_eval do
-  include Spree::CurrentVirtualOrder
-  helper_method :current_virtual_order
-
   private
   def associate_user
     return unless current_user and (current_order || current_virtual_order)
     current_order.associate_user!(current_user) if current_order
-    current_virual_order.associate_user!(current_user) if current_virual_order
+    current_virtual_order.associate_user!(current_user) if current_virtual_order
     session[:guest_token] = nil
   end
 

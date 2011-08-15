abbb8132daa8bb4067d6260a9539e7c80023dbe9
Fodoj <fodojyko@gmail.com>
2011-08-15 18:25:56 +0600

    Cart item and virtual cart item removal fixed

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,14 +3,14 @@
 
     // Remove an item from the cart by setting its quantity to zero and posting the update form
     $('form#updatecart a.delete').show().live('click', function(e){
-      $(this).parents('tr').find('input.line_item_quantity').val(0);
+      $(this).parent().find('input.txt').val(0);
       $(this).parents('form').submit();
       e.preventDefault();
     });
 
     // Remove an item from the cart by setting its quantity to zero and posting the update form
     $('form#virtual-updatecart a.delete').show().live('click', function(e){
-      $(this).parents('tr').find('input.line_item_quantity').val(0);
+      $(this).parent().find('input.txt').val(0);
       $(this).parents('form').submit();
       e.preventDefault();
     });

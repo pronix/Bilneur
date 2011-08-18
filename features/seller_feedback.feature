86663977a5dc2050e82ab04afb71d41c6a2777ac
Maxim Kremenev <ezotrank@gmail.com>
2011-08-18 19:53:47 +0300

    Buyer has views review for all sellers

--- features/seller_feedback.feature
+++ features/seller_feedback.feature
@@ -6,19 +6,32 @@ Feature: Describe buyer feedback to seller
     And the following sellers exist:
       | firstname | email              | password  | password_confirmation |
       | Seller1   | seller1@person.com | password1 | password1             |
+      | Seller2   | seller2@person.com | password2 | password2             |
+      | Seller3   | seller3@person.com | password3 | password3             |
     And the following products exist:
       | name                        |            ean | created_at | owner                    |
       | The Godfather               |  9780099528128 | 01/01/2011 | email:seller1@person.com |
     And the following variants exist:
-      | product                          | seller                   | price | condition | count_on_hand | owner                    |
-      | name:The Godfather               | email:seller1@person.com |  12.0 | new       |             6 | email:seller1@person.com |
+      | product            | seller                   | price | condition | count_on_hand | owner                    |
+      | name:The Godfather | email:seller1@person.com |  12.0 | used      |             6 | email:seller1@person.com |
+      | name:The Godfather | email:seller2@person.com |  14.0 | new       |             5 | email:seller2@person.com |
+      | name:The Godfather | email:seller3@person.com |  10.0 | new       |            10 | email:seller3@person.com |
 
   Scenario: Don't show Seller Review in buyer when it's blank
     Given I sign in as "email@person.com/password"
     Given I have order
     Then I go to the orders dashboard page
     Then I should not see "Seller Review"
-@wip
+
+  Scenario: Show feedback to buyer with some sellers
+    Given I sign in as "email@person.com/password"
+    Given I have order with all variants by product "The Godfather"
+    Given I have for each seller in order some review
+    Then I go to the orders dashboard page
+    And I should see my order
+    And I should see "Seller Review"
+    And I should see each review by seller on order
+
   Scenario: Show feedback to buyer with one seller
     Given I sign in as "email@person.com/password"
     Given I have order
--- features/step_definitions/seller_feedback_steps.rb
+++ features/step_definitions/seller_feedback_steps.rb
@@ -1,38 +1,38 @@
-Given /^I user has order with "([^\"]*)" product$/ do |product_name|
-  address = Factory(:address)
-  variant = Product.find_by_name(product_name).variants.last
-  @order = Factory(:order, :user => @user, :bill_address => address, :ship_address => address)
-  line_item = Factory(:line_item, :order => @order, :variant => variant)
-end
+# Given /^I user has order with "([^\"]*)" product$/ do |product_name|
+#   address = Factory(:address)
+#   variant = Product.find_by_name(product_name).variants.last
+#   @order = Factory(:order, :user => @user, :bill_address => address, :ship_address => address)
+#   line_item = Factory(:line_item, :order => @order, :variant => variant)
+# end
 
-Given /^I add "(.+)" shipment for order$/ do |shipment_name|
-  @order.shipping_methods << ShippingMethod.find_by_name(shipment_name)
-  @order.create_shipment!
-  @order.payment
-end
+# Given /^I add "(.+)" shipment for order$/ do |shipment_name|
+#   @order.shipping_methods << ShippingMethod.find_by_name(shipment_name)
+#   @order.create_shipment!
+#   @order.payment
+# end
 
-Then /^change order to paid$/ do
-  Order.last.payments.each { |v|
-    v.payment_source.send(:capture, v)
-  }
-end
+# Then /^change order to paid$/ do
+#   Order.last.payments.each { |v|
+#     v.payment_source.send(:capture, v)
+#   }
+# end
 
-Then /^change order to shipped$/ do
-  Order.last.shipments.each { |v|
-    v.send("ship")
-  }
-end
+# Then /^change order to shipped$/ do
+#   Order.last.shipments.each { |v|
+#     v.send("ship")
+#   }
+# end
 
-Then /^create seller review and receiving$/ do
-  seller = User.find_by_email('seller1@person.com')
-  a = SellerReview.build(Order.last, @user, seller, :review => 'adsadsads', :rating => 3)
-  a.save
-  Order.last.shipments.each { |v|
-    v.send("receiving")
-  }
-  puts a.errors
-  puts SellerReview.last.to_a
-end
+# Then /^create seller review and receiving$/ do
+#   seller = User.find_by_email('seller1@person.com')
+#   a = SellerReview.build(Order.last, @user, seller, :review => 'adsadsads', :rating => 3)
+#   a.save
+#   Order.last.shipments.each { |v|
+#     v.send("receiving")
+#   }
+#   puts a.errors
+#   puts SellerReview.last.to_a
+# end
 
 Given /^I have order$/ do
   address = Factory(:address)
@@ -40,6 +40,27 @@ Given /^I have order$/ do
   @order = Factory(:order, :user => @user, :bill_address => address, :ship_address => address)
   line_item = Factory(:line_item, :order => @order, :variant => variant)
   4.times { @order.send('next') }
+end
+
+Given /^I have order with all variants by product "(.+)"$/ do |product_name|
+  address = Factory(:address)
+  variants = Product.find_by_name(product_name).variants
+  @order = Factory(:order, :user => @user, :bill_address => address, :ship_address => address)
+  variants.each { |v| @order.line_items << Factory(:line_item, :variant => v, :order => @order) }
+  4.times { @order.send('next') }
+end
+
+Given /^I have for each seller in order some review$/ do
+  @order.line_items.each do |line_item|
+    seller = line_item.variant.seller
+    SellerReview.create(:seller => seller, :buyer => @user, :order => @order, :review => Faker::Lorem.sentences(1), :rating => rand(5))
+  end
+end
+
+Then /^I should see each review by seller on order$/ do
+  SellerReview.where(:order_id => @order.id).each do |review|
+    page.should have_content(review.seller.full_name)
+  end
 end
 
 Then /^I should see my order$/ do

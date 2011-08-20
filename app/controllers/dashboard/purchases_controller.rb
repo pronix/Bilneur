6448c17cc5de5d37a9ab9dbf566538a23d4cfe49
Fodoj <fodojyko@gmail.com>
2011-08-20 14:15:48 +0600

    Purchases dashboard page

--- app/controllers/dashboard/purchases_controller.rb
+++ app/controllers/dashboard/purchases_controller.rb
@@ -1,7 +1,7 @@
 class Dashboard::PurchasesController < Dashboard::ApplicationController
 
   def index
-    @orders = current_user.orders.complete.paginate(:per_page => (params[:pre_page]||10), :page => params[:page])
+    @orders = current_user.orders.complete.paginate(:per_page => (params[:per_page]||10), :page => params[:page])
   end
 
 end

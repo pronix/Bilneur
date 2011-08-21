41d279ee75738e5e08564626c5c9a0c686d01680
parallel588 <parallel588@gmail.com>
2011-08-21 09:40:27 +0400

    created controller for public the seller pages

---
+++ app/controllers/sellers_controller.rb
@@ -0,0 +1,23 @@
+class SellersController < Spree::BaseController
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+  helper :taxons
+  helper :products
+  respond_to :html, :js, :json
+
+  before_filter :load_data
+
+  def products
+    @quotes = @seller.quotes.paginate(:page => params[:page], :per_page => params[:per_page])
+  end
+
+  def about
+
+  end
+
+  private
+
+  def load_data
+    @seller = Role.seller.users.find(params[:id])
+  end
+
+end

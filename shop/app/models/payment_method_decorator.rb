55eee34e924d12cf1452ef0a3a2d3c25b55a035d
parallel588 <parallel588@gmail.com>
2011-08-23 11:29:32 +0400

    added paypal

---
+++ shop/app/models/payment_method_decorator.rb
@@ -0,0 +1,23 @@
+PaymentMethod.class_eval do
+  class << self
+
+    def paypal(display_on = 'both')
+      PaymentMethod.all.select { |p|
+        p.active &&
+        (p.display_on == display_on.to_s || p.display_on.blank?) &&
+        (p.environment == Rails.env || p.environment.blank?)
+      }.detect{ |v| v.name = "PayPal" }
+
+    end
+
+    def credit_card(display_on = 'both')
+      PaymentMethod.all.select { |p|
+        p.active &&
+        (p.display_on == display_on.to_s || p.display_on.blank?) &&
+        (p.environment == Rails.env || p.environment.blank?)
+      }.detect{ |v| v.name = "Credit Card" }
+
+    end
+
+  end
+end

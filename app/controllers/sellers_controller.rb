7f16772175ccec2a1218b1c24d643c3867641f88
Fodoj <fodojyko@gmail.com>
2011-08-25 00:05:40 +0600

    sidebar on seller's quote

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -13,6 +13,7 @@ class SellersController < Spree::BaseController
   def quote
     @quote = @seller.quotes.by_product_and_id(Product.find_by_permalink(params[:product_id]), params[:quote_id])
     @reviews = @quote.product.reviews.paginate_reviews(params[:per_page])
+    @seller = @quote.seller
     @product_properties = @quote.product.product_properties
   end
 

6e15475528f467c639386d886e6435ccfb791abf
parallel588 <parallel588@gmail.com>
2011-08-27 12:32:58 +0400

    fix and added edit link to quotes

--- app/controllers/dashboard/seller_inventories_controller.rb
+++ app/controllers/dashboard/seller_inventories_controller.rb
@@ -1,5 +1,5 @@
 class Dashboard::SellerInventoriesController < Dashboard::ApplicationController
   def index
-    @seller_stores = current_user.seller_stores
+    @seller_stores = current_user.seller_stores.paginate(:page => params[:page], :per_page => params[:per_page])
   end
 end

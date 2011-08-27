45ef2b8b49a4f004cd21ab20f196f84f1c984aef
Fodoj <fodojyko@gmail.com>
2011-08-27 19:21:55 +0600

    new index, header, without cart

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -9,4 +9,16 @@ module HomeHelper
     return dashboard_quotes_path if current_user.has_role?("seller")
   end
 
+  def link_to_cart(text = t('cart'))
+    return "" if current_page?(cart_path)
+    css_class = nil
+    if current_order.nil? or current_order.line_items.empty?
+      text = "#{text}: (#{t('empty')})"
+      css_class = 'empty'
+    else
+      text = "#{text}: (#{current_order.item_count}) #{order_price(current_order)}"
+      css_class = 'full'
+    end
+    link_to text, cart_path,
+  end
 end
--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
   var TabbedPanels1 = new Spry.Widget.TabbedPanels("TabbedPanels1");
   $('#carousel').infiniteCarousel({imagePath: '/images/infinitecarousel/images/'});
+  $('.cLogo,.otherbutton,.homebutton,.downloadbutton,.donatebutton, .sellbutton').append('<span class="hover"></span>').each(function () {
+    		var $span = $('> span.hover', this).css('opacity', 0);
+    		$(this).hover(function () {
+      		$span.stop().fadeTo(500, 1);
+   		}, function () {
+     	$span.stop().fadeTo(500, 0);
+    		});
+  });
+
 });

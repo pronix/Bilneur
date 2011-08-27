fb7e718aaacea98edda738892b1f2df5afddc52d
Fodoj <fodojyko@gmail.com>
2011-08-28 00:58:29 +0600

    new index - new carousel

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-  var TabbedPanels1 = new Spry.Widget.TabbedPanels("TabbedPanels1");
-  $('#carousel').infiniteCarousel({imagePath: '/images/infinitecarousel/images/'});
   $('.cLogo,.otherbutton,.homebutton,.downloadbutton,.donatebutton, .sellbutton').append('<span class="hover"></span>').each(function () {
     		var $span = $('> span.hover', this).css('opacity', 0);
     		$(this).hover(function () {
@@ -9,5 +7,4 @@ $(document).ready(function(){
      	$span.stop().fadeTo(500, 0);
     		});
   });
-
 });

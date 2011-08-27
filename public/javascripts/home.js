4ee2d9a68b60e82bfa5b204a1786e8208ffbbe6f
Fodoj <fodojyko@gmail.com>
2011-08-28 01:57:29 +0600

    new index: carousel select dropdowns

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  $("#search_select_category2, #search_select_category3").msDropDown({mainCSS:'dd2'});
   $('.cLogo,.otherbutton,.homebutton,.downloadbutton,.donatebutton, .sellbutton').append('<span class="hover"></span>').each(function () {
     		var $span = $('> span.hover', this).css('opacity', 0);
     		$(this).hover(function () {
